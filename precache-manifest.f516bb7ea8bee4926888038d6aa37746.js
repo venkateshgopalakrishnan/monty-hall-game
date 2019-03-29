4bc828272fe24166116df7a70a4fe660b9a2c542
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-03-29 10:13:18 +0530

    Updates

---
+++ precache-manifest.f516bb7ea8bee4926888038d6aa37746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d8d6ed0605b1187993",
+    "url": "/monty-hall-game/static/js/runtime~main.35762157.js"
+  },
+  {
+    "revision": "1fb18d03a6d003c17f69",
+    "url": "/monty-hall-game/static/js/main.0607a0d3.chunk.js"
+  },
+  {
+    "revision": "13cf97d8bd97b056eb7d",
+    "url": "/monty-hall-game/static/js/2.d19161d0.chunk.js"
+  },
+  {
+    "revision": "1fb18d03a6d003c17f69",
+    "url": "/monty-hall-game/static/css/main.3daa772e.chunk.css"
+  },
+  {
+    "revision": "e83f412eb1536587e8a6c2b4e703611a",
+    "url": "/monty-hall-game/index.html"
+  }
+];
