42899d16745ad254a71da312583ad43b2a62f1bf
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-03-31 03:15:34 +0530

    refactored functions

--- src/Components/GamePage/GamePage.js
+++ src/Components/GamePage/GamePage.js
@@ -22,11 +22,25 @@ class GamePage extends React.Component {
   state = {
     //  Set to false before deployment ------------->>>>>>>>>>>>>>>>>>>>>>>
     isPlayClicked: false,
+    doors: ["A", "B", "C"],
     prizes: this.shuffleArray(),
     userSelectedDoor: null,
     revealedDoor: null,
     userWon: null,
     userSelectedFinal: null
+  };
+
+  renderDoors = () => {
+    return this.state.doors.map(door => (
+      <Col xs={4}>
+        <Door
+          id={door}
+          selectedDoor={
+            !this.state.userSelectedDoor && (id => this.userSelection(id))
+          }
+        />
+      </Col>
+    ));
   };
 
   userSelection(id) {
@@ -43,27 +57,27 @@ class GamePage extends React.Component {
         ? prizes[1] === "Goat" && prizes[2] === "Goat"
           ? ["B", "C"][Math.floor(Math.random() * ["B", "C"].length)]
           : prizes[1] === "Goat"
-          ? "B"
-          : prizes[2] === "Goat"
-          ? "C"
-          : null
+            ? "B"
+            : prizes[2] === "Goat"
+              ? "C"
+              : null
         : userSelectedDoor === "B"
-        ? prizes[0] === "Goat" && prizes[2] === "Goat"
-          ? ["A", "C"][Math.floor(Math.random() * ["A", "C"].length)]
-          : prizes[0] === "Goat"
-          ? "A"
-          : prizes[2] === "Goat"
-          ? "C"
-          : null
-        : userSelectedDoor === "C"
-        ? prizes[0] === "Goat" && prizes[1] === "Goat"
-          ? ["A", "B"][Math.floor(Math.random() * ["A", "B"].length)]
-          : prizes[0] === "Goat"
-          ? "A"
-          : prizes[0] === "Goat"
-          ? "B"
-          : null
-        : null;
+          ? prizes[0] === "Goat" && prizes[2] === "Goat"
+            ? ["A", "C"][Math.floor(Math.random() * ["A", "C"].length)]
+            : prizes[0] === "Goat"
+              ? "A"
+              : prizes[2] === "Goat"
+                ? "C"
+                : null
+          : userSelectedDoor === "C"
+            ? prizes[0] === "Goat" && prizes[1] === "Goat"
+              ? ["A", "B"][Math.floor(Math.random() * ["A", "B"].length)]
+              : prizes[0] === "Goat"
+                ? "A"
+                : prizes[0] === "Goat"
+                  ? "B"
+                  : null
+            : null;
     this.state.revealedDoor = id;
     return <h2>Door {id} has a goat behind it</h2>;
   }
@@ -75,21 +89,53 @@ class GamePage extends React.Component {
           ? "C"
           : prevState.userSelectedDoor === "A" &&
             this.state.revealedDoor === "C"
-          ? "B"
-          : prevState.userSelectedDoor === "B" &&
-            this.state.revealedDoor === "A"
-          ? "C"
-          : prevState.userSelectedDoor === "B" &&
-            this.state.revealedDoor === "C"
-          ? "A"
-          : prevState.userSelectedDoor === "C" &&
-            this.state.revealedDoor === "A"
-          ? "B"
-          : prevState.userSelectedDoor === "C" &&
-            this.state.revealedDoor === "B"
-          ? "A"
-          : null
+            ? "B"
+            : prevState.userSelectedDoor === "B" &&
+              this.state.revealedDoor === "A"
+              ? "C"
+              : prevState.userSelectedDoor === "B" &&
+                this.state.revealedDoor === "C"
+                ? "A"
+                : prevState.userSelectedDoor === "C" &&
+                  this.state.revealedDoor === "A"
+                  ? "B"
+                  : prevState.userSelectedDoor === "C" &&
+                    this.state.revealedDoor === "B"
+                    ? "A"
+                    : null
     }));
+  }
+
+  declareResult() {
+    this.state.userSelectedFinal != null &&
+      (this.state.userSelectedFinal === "A" && this.state.prizes[0] === "Car"
+        ? (this.state.userWon = true)
+        : this.state.userSelectedFinal === "B" && this.state.prizes[1] === "Car"
+          ? (this.state.userWon = true)
+          : this.state.userSelectedFinal === "C" && this.state.prizes[2] === "Car"
+            ? (this.state.userWon = true)
+            : (this.state.userWon = false));
+
+    var result = this.state.userWon && this.state.userWon === true ? (
+      <h2>You won</h2>
+    ) : this.state.userWon === false ? (
+      <h2>You Lost</h2>
+    ) : null;
+
+    return result;
+  }
+
+  refreshPage = () => {
+    this.setState({
+      //  Set to false before deployment ------------->>>>>>>>>>>>>>>>>>>>>>>
+      isPlayClicked: false,
+      doors: ["A", "B", "C"],
+      prizes: this.shuffleArray(),
+      userSelectedDoor: null,
+      revealedDoor: null,
+      userWon: null,
+      userSelectedFinal: null
+    });
   }
 
   render() {
@@ -97,100 +143,59 @@ class GamePage extends React.Component {
       <div>
         {this.state.isPlayClicked === false ? (
           <button
-            class="button"
+            className="button"
             onClick={() => this.setState({ isPlayClicked: true })}
           >
             <span>Play</span>
           </button>
         ) : (
-          <div>
-            Game has started
+            <div>
+              Game has started
             <br />
-            <br />
-            <Container align="center">
-              <Row>
-                <Col xs={4}>
-                  <Door
-                    id={"A"}
-                    selectedDoor={
-                      !this.state.userSelectedDoor &&
-                      (id => this.userSelection(id))
-                    }
-                  />
-                </Col>
-                <Col xs={4}>
-                  <Door
-                    id={"B"}
-                    selectedDoor={
-                      !this.state.userSelectedDoor &&
-                      (id => this.userSelection(id))
-                    }
-                  />
-                </Col>
-                <Col xs={4}>
-                  <Door
-                    id={"C"}
-                    selectedDoor={
-                      !this.state.userSelectedDoor &&
-                      (id => this.userSelection(id))
-                    }
-                  />
-                </Col>
-              </Row>
               <br />
-              {this.state.userSelectedDoor && !this.state.userSelectedFinal && (
-                <div>
-                  <h4>
-                    Now I would like to reveal one door which has a goat behind
-                    it
+              <Container align="center">
+                <Row>{this.renderDoors()}</Row>
+                <br />
+                {this.state.userSelectedDoor && !this.state.userSelectedFinal && (
+                  <div>
+                    <h4>
+                      Now I would like to reveal one door which has a goat behind
+                      it
                   </h4>
-                  {this.revealDoor()}
-                  <h4>Do you want to change your choice to the other door</h4>
-                  <Row>
-                    <Col xg={6}>
-                      <button
-                        className="button"
-                        style={{ fontSize: "30px" }}
-                        onClick={() => this.changeChoice()}
-                      >
-                        Yes
-                      </button>
-                    </Col>
-                    <Col xg={6}>
-                      <button
-                        className="button"
-                        style={{ fontSize: "30px" }}
-                        onClick={() =>
-                          this.setState({
-                            userSelectedFinal: this.state.userSelectedDoor
-                          })
-                        }
-                      >
-                        No Way _|_
-                      </button>
-                    </Col>
-                  </Row>
-                </div>
-              )}
-              {this.state.userSelectedFinal != null &&
-                (this.state.userSelectedFinal === "A" &&
-                this.state.prizes[0] === "Car"
-                  ? (this.state.userWon = true)
-                  : this.state.userSelectedFinal === "B" &&
-                    this.state.prizes[1] === "Car"
-                  ? (this.state.userWon = true)
-                  : this.state.userSelectedFinal === "C" &&
-                    this.state.prizes[2] === "Car"
-                  ? (this.state.userWon = true)
-                  : (this.state.userWon = false))}
-              {this.state.userWon && this.state.userWon === true ? (
-                <h2>You won</h2>
-              ) : this.state.userWon === false ? (
-                <h2>You Lost</h2>
-              ) : null}
-            </Container>
-          </div>
-        )}
+                    {this.revealDoor()}
+                    <h4>Do you want to change your choice to the other door</h4>
+                    <Row>
+                      <Col xg={6}>
+                        <button
+                          className="button"
+                          style={{ fontSize: "30px" }}
+                          onClick={() => this.changeChoice()}
+                        >
+                          <span>Yes</span>
+                        </button>
+                      </Col>
+                      <Col xg={6}>
+                        <button
+                          className="button"
+                          style={{ fontSize: "30px" }}
+                          onClick={() =>
+                            this.setState({
+                              userSelectedFinal: this.state.userSelectedDoor
+                            })
+                          }
+                        >
+                          <span>No Way _|_</span>
+                        </button>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+                {this.declareResult()}
+                {this.state.userWon != null && <button className="button" onClick={() => { this.refreshPage() }}><span>Play Again</span></button>}
+
+              </Container>
+            </div>
+          )}
       </div>
     );
   }
